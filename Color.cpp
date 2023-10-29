313ff897927c3a53704c2084b96bfe1e084a956e
Tree-B <147557573+Tree-B@users.noreply.github.com>
2023-10-29 16:45:56 -0400

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -12,55 +12,77 @@
  */
 
 #include "Color.h"
+const int COLOR_MAX = 255;
 
 // TODO: implement first checkRange, then two constructors, setRed, getRed,
 //       setGreen, getGreen, setBlue, getBlue, read, write.
 
-Color::Color() {
-   // to do - implement
+int Color::checkRange(int val){
+    if ((val >= 0) && (val < COLOR_MAX)){
+        return val;
+    }
+    else{
+        if(val < 0){
+            return 0;
+        }
+        else{
+            return (COLOR_MAX);
+        }
+    }
 }
 
-Color::Color(int redVal, int greenVal, int blueVal) {
-   // to do - implement
+Color::Color(){
+    red = 0;
+    green = 0;
+    blue = 0;
 }
 
-void Color::setRed(int redVal) {
-   red = checkRange(redVal);
+Color::Color(int redVal, int greenVal, int blueVal){
+    checkRange(redVal);
+    checkRange(greenVal);
+    checkRange(blueVal);
+    //i'm not sure if this needs anything else
 }
 
-int Color::getRed() {
-   return red;
+void Color::setRed(int redVal){
+    red = checkRange(redVal);
 }
 
-void Color::setGreen(int greenVal) {
-   green = checkRange(greenVal);
+int Color::getRed(){
+    return red;
 }
 
-int Color::getGreen() {
-   return green;
+void Color::setGreen(int greenVal){
+    green = checkRange(greenVal);
 }
 
-void Color::setBlue(int blueVal) {
-   blue = checkRange(blueVal);
+int Color::getGreen(){
+    return green;
 }
 
-int Color::getBlue() {
-   return blue;
+void Color::setBlue(int blueVal){
+    blue = checkRange(blueVal);
 }
 
-void Color::read(istream& ins) {
-   // to do - implement
+int Color::getBlue(){
+    return blue;
 }
 
-void Color::write(ostream& outs) {
-   // to do - implement
+void Color::read(istream& ins){
+    ins >>  red >> green >> blue;
+    red = checkRange(red);
+    green = checkRange(green);
+    blue = checkRange(blue);
+    return;
 }
 
-int Color::checkRange(int val) {
-    // to do - implement
-
-    // to do - replace with correct return statement
-    return val;
+void Color::write(ostream& outs){
+    outs << red << " " << green << " " << blue;
+    red = checkRange(red);
+    green = checkRange(green);
+    blue = checkRange(blue);
+    outs << endl;
+    return;
 }
 
 // Your code goes above this line.
