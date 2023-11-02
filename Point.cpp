9c09b9bf09a1ff05ec5928ee47beab80c3ac611c
Alexandria Balde <66260572+alxndriab@users.noreply.github.com>
2023-11-01 20:35:33 -0400

    updated point

--- Point.cpp
+++ Point.cpp
@@ -1,12 +1,12 @@
 /**
  * Point.cpp
+ * Project UID 2e6ea4e086ea6a06753e819c30923369
  *
  * EECS 183
- * Lab 7
- * Taken from Project 4: CoolPics
+ * Project 4: CoolPics
  *
- * Teresa Billecke
- * tbilleck
+ * <#Name(s)#>
+ * <#uniqname(s)#>
  *
  * <#Description#>
  */
@@ -16,82 +16,59 @@
 // for the declaration of DIMENSION
 #include "utility.h"
 
-// TODO: implement two constructors, setX, getX, setY, getY, read, write, checkRange.
 Point::Point() {
-    // TODO: Implement
     x = 0;
     y = 0;
 }
-    
+
 Point::Point(int xVal, int yVal) {
-    // TODO: Implement
     x = checkRange(xVal);
     y = checkRange(yVal);
 }
 
 void Point::setX(int xVal) {
-    // TODO: Implement
-    x = xVal;
-    x = checkRange(x);
-    return;
+    x = checkRange(xVal);
 }
 
 int Point::getX() {
-    // TODO: Implement
-    // FIXME: returning arbitrary value of 0 to avoid compile error
     return x;
 }
 
 void Point::setY(int yVal) {
-    // TODO: Implement
-    y = yVal;
-    y = checkRange(y);
-    return;
+    y = checkRange(yVal);
 }
-    
+
 int Point::getY() {
-    // TODO: Implement
-    // FIXME: returning arbitrary value of 0 to avoid compile error
     return y;
 }
 
 void Point::read(istream& ins) {
-    // TODO: Implement
-    char trash;
-    ins >> trash >> x >> trash >> y >> trash;
-    x = checkRange(x);
-    y = checkRange(y);
-    return;
+    char junk;
+    int xVal;
+    int yVal;
+    
+    ins >> junk >> xVal >> junk >> yVal >> junk;
+    setX(xVal);
+    setY(yVal);
 }
 
 void Point::write(ostream& outs) {
-    // TODO: Implement
-    outs << '(' << x << ','<< y << ')';
-    x = checkRange(x);
-    y = checkRange(y);
-    outs << endl;
-    return;
+    outs << '(' << x << ',' << y << ')';
 }
 
 int Point::checkRange(int val) {
-    // TODO: Implement
-    if ((val >= 0) && (val < DIMENSION)){
+    if (val >= 0 && val < DIMENSION) {
         return val;
     }
-    else{
-        if(val < 0){
-            return 0;
-        }
-        else{
-            return (DIMENSION-1);
-        }
+    else if (val < 0) {
+        return 0;
     }
-    // FIXME: returning arbitrary value of 0 to avoid compile error
+    return DIMENSION - 1;
 }
 
 
-// Your code goes above this line!!!
-// Don't change the implementations below!!!
+// Your code goes above this line.
+// Don't change the implementations below!
 
 istream& operator >> (istream& ins,  Point& pt)
 {
